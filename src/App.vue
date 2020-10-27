e9a7d0c24b69f90a35ae78a574276cfa5a990f95
Oketa Fred <oketafred@gmail.com>
2020-10-27 10:15:03 +0300

    Added Tailwind CSS

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="text-center align-middle">
+    <img class="mx-auto" alt="Vue logo" src="./assets/logo.png" />
+    <h1 class="text-4xl font-semibold">Tailwind CSS and Vue JS Version 3</h1>
+    <HelloWorld msg="Welcome to Your Vue.js and Tailwind App" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/tailwind.css"/>
